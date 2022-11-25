bbc11d0861381dcacd35ca183b8ec70764e75b5e
Saulius <sauliusnesukaitis@gmail.com>
2022-11-25 21:59:37 +0200

    Conditional statements and Data types

---
+++ app.js
@@ -0,0 +1,23 @@
+let a = 'abc';
+let b = true;
+let c = 1;
+
+console.log(a+b)
+console.log(a+c)
+console.log(b+c)
+
+console.log(b*c)
+console.log(a*c)
+console.log(b*c)
+
+console.log(b/c)
+console.log(a/c)
+console.log(b/c)
+
+let x = Number(a);
+let y = String(b);
+let z = Boolean(c);
+
+console.log(a)
+console.log(b)
+console.log(c)
