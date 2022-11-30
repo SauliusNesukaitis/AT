33bfcb55bf96003ecc688449140662f0bdfac947
Saulius <sauliusnesukaitis@gmail.com>
2022-11-30 19:09:25 +0200

    Arrays and Cycles

--- app.js
+++ app.js
@@ -69,3 +69,99 @@ function isPrime(num) {
 }
 
 console.log(isPrime(1))
+
+
+// Arrays and Cycles
+
+// 1
+
+let movieNames = ["movie1", "movie2", "movie3"];
+
+movieNames.forEach(movie => console.log(movie));
+
+// 2
+
+let carManufacturers = ["car1", "car2", "car3"];
+
+let carString = carManufacturers.toString();
+
+let carArray = carString.split(",");
+
+console.log(carArray);
+
+// 3
+
+let names = ["name1", "name2", "name3"];
+
+let namesHello = names.map(name => "Hello " + name);
+
+console.log(namesHello);
+
+// 4
+
+let numericArray = [0, 1, 0, 0];
+
+let booleanArray = [];
+
+for (let i = 0; i < numericArray.length; i++) {
+    booleanArray.push(Boolean(numericArray[i]));
+}
+
+console.log(booleanArray);
+
+// 5
+
+let arr = [1,6,7,8,3,4,5,6];
+
+let sortedArray = arr.sort((a, b) => b - a);
+
+console.log(sortedArray);
+
+// 6
+
+let arr1 = [1,6,7,8,3,4,5,6];
+
+let filteredArray = arr1.filter((a) => a > 3);
+
+console.log(filteredArray);
+
+// 7
+
+let arr2 = [1,6,7,8,3,4,5,6];
+
+function indexOfNumber(arr, num) {
+    console.log(arr.indexOf(num))
+}
+
+indexOfNumber(arr2, 5)
+
+// 8
+
+let i = 18;
+
+while (i > 10) {
+    i--
+    console.log(i)
+}
+
+// 9
+
+for(let number = 1 ; number <= 20; number++){
+    let isPrime = true;
+
+    for (let i = 2; i <= number - 1 ;i++){
+        if(number % i === 0){
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) console.log(number);
+}
+
+// 10
+
+for(let number = 1 ; number <= 20; number++){
+    if (number % 2 === 0){
+    console.log(number);
+    }
+}
